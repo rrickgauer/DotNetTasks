149a2ecf39f7cfe496a3aa7dd6415c08a5247358
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-07-12 10:31:03 -0500

    setup visual studio solution

---
+++ src/Tasks/Tasks/Controllers/EventsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tasks.Controllers
+{
+    [ApiController]
+    [Route("events")]
+    public class EventsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> GetEvents()
+        {
+            return Ok("Get all events");
+        }
+    }
+}
