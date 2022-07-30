533ef61f2dbcf6ec0773650fc44382454b8349fe
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-07-30 12:23:56 -0500

    make a service call that determines which delete action to do

--- src/Gui/tasks/services/events.py
+++ src/Gui/tasks/services/events.py
@@ -1,10 +1,19 @@
 from __future__ import annotations
+from datetime import date
+from enum import Enum, auto
 from uuid import UUID
 import flask
 import requests
 from tasks.config.routines import get_config
 from tasks.common import security
 from tasks.common.structs import BaseReturn
+from tasks.common.url_rules import DeleteEventUrlRules
+
+
+class EventDeleteAction(Enum):
+    ALL       = auto()
+    SINGLE    = auto()
+    FOLLOWING = auto()
 
 #------------------------------------------------------
 # Send an api request to update an event using the data gathered from the request form
@@ -20,6 +29,9 @@ def update_event_from_request(event_id: UUID) -> requests.Response:
     return response
 
 
+#------------------------------------------------------
+# Get the specified event from the api
+#------------------------------------------------------
 def get_event(event_id: UUID) -> BaseReturn:
     result = BaseReturn(successful=True)
 
@@ -49,3 +61,32 @@ def _build_api_event_url(event_id) -> str:
     url = f'{config.URL_API}/events/{event_id}'
 
     return url
+
+
+#------------------------------------------------------
+# Get the appropriate delete event action by examining the request url
+#------------------------------------------------------
+def get_delete_action() -> EventDeleteAction | None:
+    url_rule = flask.request.url_rule.rule
+
+    if url_rule == f'/api/{DeleteEventUrlRules.ALL}':
+        return EventDeleteAction.ALL
+    elif url_rule == f'/api/{DeleteEventUrlRules.SINGLE}':
+        return EventDeleteAction.SINGLE
+    elif url_rule == f'/api/{DeleteEventUrlRules.FOLLOWING}':
+        return EventDeleteAction.FOLLOWING
+    else:
+        return None
+
+
+def delete_event(event_id: UUID) -> BaseReturn:
+    result = BaseReturn(successful=True)
+
+    return result
+
+
+def delete_event_occurence(event_id: UUID, date_val: date):
+    return 'delete_event_occurence'
+
+def delete_event_occurence_following(event_id: UUID, date_val: date):
+    return 'delete_event_occurence_following'
