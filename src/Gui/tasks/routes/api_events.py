789acff3161ef17ff32a96b4ce79420390bb3516
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-08-18 14:03:19 -0500

    write logic for deleting the event and remaining occurences

--- src/Gui/tasks/routes/api_events.py
+++ src/Gui/tasks/routes/api_events.py
@@ -9,7 +9,9 @@ api endpoints
 """
 
 from __future__ import annotations
+from dataclasses import asdict
 from datetime import date
+import json
 from uuid import UUID
 import flask
 from tasks.common import security
@@ -35,7 +37,7 @@ def modify_event(event_id: UUID):
 @bp_api_events.get('<uuid:event_id>')
 @security.login_required
 def get_event(event_id: UUID):
-    result = services.events.get_event(event_id)
+    result = services.events.send_get_request(event_id)
 
     if not result.successful:
         return (str(result.error), HTTPStatus.BAD_REQUEST)
@@ -66,17 +68,38 @@ def delete_single_occurence(event_id: UUID, date_val: date=None):
     if not result.successful:
         return (str(result.error), HTTPStatus.BAD_REQUEST)
 
-    return ('', HTTPStatus.OK)
+    return ('', HTTPStatus.NO_CONTENT)
+
 
 #------------------------------------------------------
 # DELETE: /api/events/:event_id/:date/remaining
+#
+# 1. Cancel the recurrence on the given date
+# 2. Modify the event so that it ends on the given date
 #------------------------------------------------------
 @bp_api_events.delete('<uuid:event_id>/<date:date_val>/remaining')
 @security.login_required
-def delete_occurence_and_following(event_id: UUID, date_val: date=None):
-    result = services.events.delete_event(event_id)
+def delete_occurence_and_following(event_id: UUID, date_val: date):
+    # create a cancellation record on the given occurence date
+    cancellation_request_result = services.cancellations.create_cancellation(event_id, date_val)
 
-    if not result.successful:
-        return (str(result.error), HTTPStatus.BAD_REQUEST)
+    if not cancellation_request_result.successful:
+        return (str(cancellation_request_result.error), HTTPStatus.BAD_REQUEST)
+
+    # fetch the event model from the api
+    get_event_request_result = services.events.get_event_model(event_id)
+
+    if not get_event_request_result.successful:
+        return (str(get_event_request_result.error), HTTPStatus.BAD_REQUEST)
+
+    # set the endsOn value to occurence date
+    event_updated = get_event_request_result.data
+    event_updated.endsOn = date_val
+
+    # send the updated event to the api to save it to the database
+    update_request_response = services.events.send_put_request(event_id, asdict(event_updated))
+
+    if not update_request_response.ok:
+        return (update_request_response.text, update_request_response.status_code)
 
     return ('', HTTPStatus.NO_CONTENT)
--- src/Gui/tasks/services/events.py
+++ src/Gui/tasks/services/events.py
@@ -7,39 +7,86 @@ Service methods for events
 """
 
 from __future__ import annotations
+from dataclasses import dataclass
 from uuid import UUID
 import flask
 import requests
+from tasks.domain.models.api_responses import EventApiResponse
 from tasks.common.api_url_builder import ApiUrlBuilder
 from tasks.common import security
 from tasks.common.structs import BaseReturn
+from tasks.common import serializers
+
+@dataclass
+class GetEventApiResponse(BaseReturn):
+    data: str = None
+
+
+@dataclass
+class GetEventModelResult(BaseReturn):
+    data: EventApiResponse | None = None
+
 
 #------------------------------------------------------
 # Send an api request to update an event using the data gathered from the request form
 #------------------------------------------------------
 def update_event_from_request(event_id: UUID) -> requests.Response:
+    return send_put_request(event_id, flask.request.form)
+
+#------------------------------------------------------
+# Send a PUT request to the api for the specified event using the given event data
+#------------------------------------------------------
+def send_put_request(event_id, event_data) -> requests.Response:
     response = requests.put(
         verify = False,
         auth   = security.get_user_session_tuple(),
         url    = _build_api_event_url(event_id),
-        data   = flask.request.form,
+        data   = event_data,
     )
 
     return response
+
+#------------------------------------------------------
+# Get an event from the api
+#------------------------------------------------------
+def get_event_model(event_id: UUID) -> GetEventModelResult:
+    # fetch the data from the api
+    api_response = send_get_request(event_id)
+
+    if not api_response.successful:
+        return api_response
+
+    result = GetEventModelResult(successful=True)
+
+    try:
+        # parse the json string into a dict
+        json_object = flask.json.loads(api_response.data)
+
+        # serialize the dict into a domain model
+        serializer = serializers.EventApiResponseSerializer(json_object)
+        model = serializer.serialize()
+
+        result.data = model
+    
+    except Exception as ex:
+        result.successful = False
+        result.error = ex
+
+    return result
 
 
 #------------------------------------------------------
 # Get the specified event from the api
 #------------------------------------------------------
-def get_event(event_id: UUID) -> BaseReturn:
-    result = BaseReturn(successful=True)
+def send_get_request(event_id: UUID) -> GetEventApiResponse:
+    result = GetEventApiResponse(successful=True)
 
-    url = _build_api_event_url(event_id)
+    # url = _build_api_event_url(event_id)
 
     response = requests.get(
         verify = False,
         auth   = security.get_user_session_tuple(),
-        url    = url,
+        url    = _build_api_event_url(event_id),
     )
 
     if not response.ok:
