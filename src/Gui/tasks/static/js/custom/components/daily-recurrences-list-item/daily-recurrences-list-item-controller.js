ad70024d6e4c63ee925d3c8f85fb0a574d01a21b
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-08-19 08:47:18 -0500

    add the delete api calls

--- src/Gui/tasks/static/js/custom/components/daily-recurrences-list-item/daily-recurrences-list-item-controller.js
+++ src/Gui/tasks/static/js/custom/components/daily-recurrences-list-item/daily-recurrences-list-item-controller.js
@@ -2,9 +2,12 @@
 ////@ts-check
 
 import { ApiCompletions } from "../../api/api-completion";
+import { ApiEvents } from "../../api/api-events";
 import { DailyRecurrencesListItemDropdownBtnActions } from "../../domain/enums/daily-recurrences-list-item-dropdown-actions";
 import { DailyRecurrenceCard } from "../daily-recurrences-card/daily-recurrences-card-element";
 import { EventModal } from "../event-modal/event-modal";
+import { AlertPageTopSuccess } from "../page-alerts/alert-page-top";
+import { RecurrencesBoardActionsController } from "../recurrences-board/recurrences-board-controller";
 import { DailyRecurrencesListItemElement } from "./daily-recurrences-list-item-element";
 
 
@@ -16,6 +19,8 @@ export class DailyRecurrencesListItemController
     constructor()
     {
         this.eventModal = new EventModal();
+        this.apiEvents = new ApiEvents();
+        this.boardController = new RecurrencesBoardActionsController();
     }
 
 
@@ -127,7 +132,7 @@ export class DailyRecurrencesListItemController
      * Handle a dropdown menu button click event
      * @param {HTMLButtonElement} eClickedBtn the clicked button
      */
-    _handleDropdownMenuBtnClick = (eClickedBtn) =>
+    _handleDropdownMenuBtnClick = async (eClickedBtn) =>
     {
         const listItem = this._getListItemFromChild(eClickedBtn);
         const actionValue = this._getDropdownMenuBtnActionValue(eClickedBtn);
@@ -135,11 +140,11 @@ export class DailyRecurrencesListItemController
         switch(actionValue)
         {
             case DailyRecurrencesListItemDropdownBtnActions.DELETE_THIS_EVENT:
-                alert('delete this event');
+                this._deleteThisEvent(listItem);
                 break;
 
             case DailyRecurrencesListItemDropdownBtnActions.DELETE_THIS_AND_FOLLOWING_EVENTS:
-                alert('delete this and following events');
+                this._deleteThisEventAndFollowing(listItem);
                 break;
         }
     }
@@ -157,11 +162,62 @@ export class DailyRecurrencesListItemController
         return parseInt(value);
     }
 
-
     //#endregion
 
+    //#region Delete event
+
+    /**
+     * Cancel the event
+     * @param {DailyRecurrencesListItemElement} listItem the list item to delete
+     */
+    _deleteThisEvent = async (listItem) =>
+    {
+        this._sendDeleteRequest(listItem, this.apiEvents.deleteThisEvent);
+    }
 
 
+    /**
+     * Delete the event on the occurence date and the following
+     * @param {DailyRecurrencesListItemElement} listItem the list item to delete
+     */
+    _deleteThisEventAndFollowing = async (listItem) =>
+    {
+        this._sendDeleteRequest(listItem, this.apiEvents.deleteThisEventAndFollowing);
+    }
+
+    /**
+     * Send a delete request using the given api callback method
+     * @param {DailyRecurrencesListItemElement} listItem the list item to delete
+     * @param {CallableFunction} apiCallback the EventsApi function to call
+     */
+    _sendDeleteRequest = async (listItem, apiCallback) =>
+    {
+        const response = await apiCallback(listItem.eventId, listItem.occurenceDate);
+
+        if (response.ok)
+        {
+            this._handleSuccessfulDeleteRequest();
+        }
+        else
+        {
+            console.error(await response.text());
+        }
+    }
+
+    /**
+     * Steps to take after a successful DELETE event api request
+     */
+    _handleSuccessfulDeleteRequest = async () =>
+    {
+        // refresh the recurrences board
+        await this.boardController.getWeeklyRecurrences();
+
+        // notify user that the event was successfully deleted
+        const alertTop = new AlertPageTopSuccess('Event was deleted successfully.');
+        alertTop.show();
+    }
+
+    //#endregion
 
 
     //#region Additional utilities
