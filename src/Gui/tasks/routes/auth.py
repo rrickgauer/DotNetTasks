a3dfdf38ef326a1573970f8caf15589aa3984e01
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-08-22 17:11:16 -0500

    clear out session values when going to auth page

--- src/Gui/tasks/routes/auth.py
+++ src/Gui/tasks/routes/auth.py
@@ -3,23 +3,25 @@
 
 Url Prefix: /auth
 
-test routes
-
 ********************************************************************************************
 """
 
 from __future__ import annotations
 import flask
+from tasks.common import security
 
 # module blueprint
 bp_auth = flask.Blueprint('auth', __name__)
 
 #------------------------------------------------------
-# Home page
-# tickle.com
+# tickle.com/auth
+# tickle.com/auth/login
 #------------------------------------------------------
 @bp_auth.route('')
 @bp_auth.route('login')
 def login():
+    # clear out the session values
+    security.clear_session_values()
+
     return flask.render_template('pages/auth/login.html')
     
