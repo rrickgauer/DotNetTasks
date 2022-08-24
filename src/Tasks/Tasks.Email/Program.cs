a86c276f9bb1c571efb512fe12e2a9603cb051b6
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-08-24 16:37:56 -0500

    add email project

---
+++ src/Tasks/Tasks.Email/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tasks.Email
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
