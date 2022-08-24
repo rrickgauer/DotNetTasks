285d0e722a3d78a6a9b33cabcf8717686992887f
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-08-24 08:26:08 -0500

    create a response object to return from the endpoint

---
+++ src/Tasks/Tasks/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tasks.Configurations;
+using Tasks.Domain.Models;
+using Tasks.Domain.Parms;
+using Tasks.Domain.Views;
+using Tasks.Security;
+using Tasks.Services.Interfaces;
+
+namespace Tasks.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("user")]
+    public class UserController : ControllerBase
+    {
+        #region Private members
+        private readonly IConfigs _configuration;
+        private readonly IUserServices _userServices;
+        private Guid CurrentUserId => SecurityMethods.GetUserIdFromRequest(Request).Value;
+        #endregion
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <param name="userServices"></param>
+        public UserController(IConfigs configuration, IUserServices userServices)
+        {
+            _configuration = configuration;
+            _userServices = userServices;
+        }
+
+        /// <summary>
+        /// POST: /user/signup
+        /// 
+        /// DOES NOT NEED AUTH HEADER!!
+        /// 
+        /// </summary>
+        /// <param name="signUpRequest"></param>
+        /// <returns></returns>
+        [AllowAnonymous]
+        [HttpPost("signup")]
+        public async Task<ActionResult<SignupRequestResponse>> SignUp([FromForm] SignUpRequest signUpRequest)
+        {
+            SignupRequestResponse result = new()
+            {
+                Successful = true,
+                User = new(),
+            };
+
+            return Ok(result);
+        }
+
+    }
+}
---
+++ src/Tasks/Tasks/Domain/Views/SignupRequestResponse.cs
@@ -0,0 +1,11 @@
+﻿using Tasks.Domain.Models;
+
+namespace Tasks.Domain.Views
+{
+    public class SignupRequestResponse
+    {
+        public bool Successful { get; set; }
+        public string? Error { get; set; }
+        public User? User { get; set; }
+    }
+}
