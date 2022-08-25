21fbd9ca6e6973744d151e794d877c6dd6b57dd9
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-08-25 13:12:21 -0500

    Add better looking update messages

--- scripts/vps/.deploy-gui.sh
+++ scripts/vps/.deploy-gui.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
+
+printf "\n\n\n"
+echo "------------------------------------------------"
+echo "Installing any new python dependencies..."
+echo "------------------------------------------------"
+printf "\n\n\n"
+
 # Install any new python dependencies
 cd /var/www/DotNetTasks/src/Gui
 /var/www/DotNetTasks/src/Gui/.venv/bin/pip install -r requirements.txt
+
+
+printf "\n\n\n"
+echo "------------------------------------------------"
+echo "Compiling CSS files..."
+echo "------------------------------------------------"
+printf "\n\n\n"
 
 # Compile sass
 cd /var/www/DotNetTasks/src/Gui/tasks/static/css
 sass --quiet --no-source-map custom/style.scss dist/custom/style.css
 sass --quiet --no-source-map custom/custom-bootstrap.scss dist/custom/bootstrap.css
+
+
+printf "\n\n\n"
+echo "------------------------------------------------"
+echo "Compiling JavaScript files..."
+echo "------------------------------------------------"
+printf "\n\n\n"
 
 # Compile js
 cd /var/www/DotNetTasks/src/Gui/tasks/static/js
