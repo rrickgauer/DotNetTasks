96cf470dd407513f61d5e08f611f459b29a4f3a8
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-08-30 19:39:57 -0500

    setup new console application

---
+++ src/Tasks/Tasks.Reminders/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+
+using CommandLine;
+using Tasks.Reminders;
+
+Console.WriteLine("sup bro");
+
+var cliArgs = Parser.Default.ParseArguments<TasksRemindersCliArgs>(args).Value;
+
+int x = 10;
+
+
+
+
+
+
---
+++ src/Tasks/Tasks.Reminders/TasksRemindersCliArgs.cs
@@ -0,0 +1,11 @@
+﻿
+using CommandLine;
+
+namespace Tasks.Reminders;
+
+public class TasksRemindersCliArgs
+{
+    [Option('d', "development", Required = false, HelpText = "Run in development.")]
+    public bool Development { get; set; }
+
+}
