2b77e4653780d5c3974f4ab4939f4779e48cc893
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-09-02 13:41:05 -0500

    create a macro that generates the html for the labels

--- src/Gui/tasks/static/js/custom/pages/labels/index.js
+++ src/Gui/tasks/static/js/custom/pages/labels/index.js
@@ -28,5 +28,8 @@ async function getLabelsHtml()
     const response = await api.get();
     const data = await response.text();
 
+
+    document.getElementById('labels-wrapper-spinner').classList.add('d-none');
+
     $('#labels-content').html(data);
 }
