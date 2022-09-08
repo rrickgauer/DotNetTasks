28522cee3e49390e40618287967dc5f043df89cd
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-09-08 17:19:40 -0500

    clean up

--- src/Gui/tasks/services/recurrences.py
+++ src/Gui/tasks/services/recurrences.py
@@ -38,8 +38,7 @@ def get_recurrences(week_range: models.WeekRange) -> GetRecurrencesResult:
 def _get_recurrences_from_api(week_range: models.WeekRange) -> requests.Response:
     # setup url
     config = get_config()
-    # api_url   = f'{config.URL_API}/recurrences'
-    api_url   = f'{config.URL_API}/recurrences/test-all'
+    api_url   = f'{config.URL_API}/recurrences'
     
     # setup url query parms
     parms = dict(
--- src/Tasks/Tasks.Services/Interfaces/IRecurrenceServices.cs
+++ src/Tasks/Tasks.Services/Interfaces/IRecurrenceServices.cs
@@ -7,21 +7,8 @@ namespace Tasks.Services.Interfaces
 {
     public interface IRecurrenceServices
     {
-        //public Task<List<Recurrence>> GetRecurrencesAsync(RecurrenceRetrieval recurrenceRetrieval);
-        //public Task<List<Recurrence>> GetEventRecurrencesAsync(EventRecurrenceRetrieval eventRecurrenceRetrieval);
-
-        public Task<IEnumerable<Recurrence>> GetRecurrencesAsync(RecurrenceRetrieval recurrenceRetrieval);
-        public Task<IEnumerable<Recurrence>> GetEventRecurrencesAsync(EventRecurrenceRetrieval eventRecurrenceRetrieval);
-
         public Task<IEnumerable<Recurrence>> GetRecurrencesForRemindersAsync(IValidDateRange validDateRange);
-
-
-
-        public Task<IEnumerable<GetRecurrencesResponse>> GetRecurrencesAsync_NEW(RecurrenceRetrieval recurrenceRetrieval);
-
-
-
-
-
+        public Task<IEnumerable<GetRecurrencesResponse>> GetRecurrencesAsync(RecurrenceRetrieval recurrenceRetrieval);
+        public Task<IEnumerable<GetRecurrencesResponse>> GetRecurrencesAsync(EventRecurrenceRetrieval recurrenceRetrieval);
     }
 }
