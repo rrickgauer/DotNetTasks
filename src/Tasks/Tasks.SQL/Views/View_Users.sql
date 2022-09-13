847827ac57e02fc86606fc759452c0123800f8b3
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-09-13 15:33:06 -0500

    Branch issue 125 (#126)
    * update layout
    
    * Update View_Users.sql

--- src/Tasks/Tasks.SQL/Views/View_Users.sql
+++ src/Tasks/Tasks.SQL/Views/View_Users.sql
@@ -1,14 +1,25 @@
-CREATE VIEW View_Users AS
+CREATE ALGORITHM = UNDEFINED DEFINER = `main` @ `%` SQL SECURITY DEFINER VIEW `View_Users` AS
 SELECT
-    u.id AS id,
-    u.email AS email,
-    u.password AS password,
-    u.created_on AS created_on,
-    u.deliver_reminders AS deliver_reminders,
-    v.confirmed_on AS email_confirmed_on
+    `u`.`id` AS `id`,
+    `u`.`email` AS `email`,
+    `u`.`password` AS `password`,
+    `u`.`created_on` AS `created_on`,
+    `u`.`deliver_reminders` AS `deliver_reminders`,
+    (
+        SELECT
+            `v`.`created_on`
+        FROM
+            `User_Email_Verifications` `v`
+        WHERE
+            (
+                (`v`.`user_id` = `u`.`id`)
+                AND (`v`.`email` = `u`.`email`)
+                AND (`v`.`confirmed_on` IS NOT NULL)
+            )
+        LIMIT
+            1
+    ) AS `email_confirmed_on`
 FROM
-    Users u
-    LEFT JOIN User_Email_Verifications v ON v.user_id = u.id
-    AND v.email = u.email
+    `Users` `u`
 GROUP BY
-    u.id;
+    `u`.`id`;
