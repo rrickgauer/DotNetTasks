bf8973383dcd67b3ada654f353989213a5325fe6
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-09-13 07:06:53 -0500

    Update export-tasks-data.sh

--- scripts/sql/export-tasks-data.sh
+++ scripts/sql/export-tasks-data.sh
@@ -1,11 +1,11 @@
 # https://dev.mysql.com/doc/refman/8.0/en/mysqldump.html
 
 mysqldump   \
---databases=Tasks_Dev   \
+--databases Tasks_Dev \
 --user=main     \
 --column-statistics=FALSE   \
 --password  \
 --skip-comments     \
 --replace   \
---result-file=tasks-data-dump.sql 
+--result-file tasks-data-dump.sql 
 
