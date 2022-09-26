263d34d6ecab7765013a49e26c6e464a24b493b6
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-09-26 15:29:06 -0500

    add the new project

---
+++ src/Tasks/Tasks.WpfUi/Models/AppConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace Tasks.WpfUi.Models
+{
+    public class AppConfig
+    {
+        public string ConfigurationsFolder { get; set; }
+
+        public string AppPropertiesFileName { get; set; }
+    }
+}
