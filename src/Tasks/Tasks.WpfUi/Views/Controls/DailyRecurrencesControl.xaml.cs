a44000c00e85253ea019a21c032fde3207525cc4
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-09-27 10:07:58 -0500

    setup a new control for the daily recurrences

---
+++ src/Tasks/Tasks.WpfUi/Views/Controls/DailyRecurrencesControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Tasks.WpfUi.ViewModels;
+
+namespace Tasks.WpfUi.Views.Controls;
+
+/// <summary>
+/// Interaction logic for DailyRecurrencesControl.xaml
+/// </summary>
+public partial class DailyRecurrencesControl : UserControl
+{
+    public DailyRecurrencesViewModel ViewModel { get; set; }
+
+    public DailyRecurrencesControl(DailyRecurrencesViewModel viewModel)
+    {
+        ViewModel = viewModel;
+
+        InitializeComponent();
+    }
+}
