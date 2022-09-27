adcb15db0646a9c99f6f0baae92ab66e92a78f25
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-09-27 13:26:10 -0500

    toggle occurrence completion

--- src/Tasks/Tasks.Services/Interfaces/IEventActionServices.cs
+++ src/Tasks/Tasks.Services/Interfaces/IEventActionServices.cs
@@ -1,15 +1,12 @@
 ﻿using Tasks.Domain.Models;
 using Tasks.Domain.Parms;
 
-namespace Tasks.Services.Interfaces
+namespace Tasks.Services.Interfaces;
+
+public interface IEventActionServices
 {
-    public interface IEventActionServices
-    {
-        public Task<EventAction> SaveEventCompletionAsync(Guid eventId, DateTime onDate);
-        public Task<bool> DeleteEventCompletionAsync(Guid eventId, DateTime onDate);
-        public Task<EventAction?> GetEventCompletionAsync(Guid eventId, DateTime onDate);
-
-
-        public Task<EventAction> SaveEventCancellationAsync(Guid eventId, DateTime onDate);
-    }
+    public Task<EventAction> SaveEventCompletionAsync(Guid eventId, DateTime onDate);
+    public Task<bool> DeleteEventCompletionAsync(Guid eventId, DateTime onDate);
+    public Task<EventAction?> GetEventCompletionAsync(Guid eventId, DateTime onDate);
+    public Task<EventAction> SaveEventCancellationAsync(Guid eventId, DateTime onDate);
 }
--- src/Tasks/Tasks.WpfUi/ViewModels/DailyRecurrencesViewModel.cs
+++ src/Tasks/Tasks.WpfUi/ViewModels/DailyRecurrencesViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Tasks.Domain.Views;
+using Tasks.Services.Interfaces;
 
 namespace Tasks.WpfUi.ViewModels;
 
@@ -19,9 +21,53 @@ public partial class DailyRecurrencesViewModel : ObservableObject
     [ObservableProperty]
     private bool _isExpanded = true;
 
+    [ObservableProperty]
+    private GetRecurrencesResponse? _selectedRecurrence;
+
+
+    private readonly IEventActionServices _eventActionServices = App.GetService<IEventActionServices>();
+
+    /// <summary>
+    /// Empty constructor
+    /// </summary>
+    public DailyRecurrencesViewModel() 
+    {
+        
+    }
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="date"></param>
+    /// <param name="recurrences"></param>
     public DailyRecurrencesViewModel(DateTime date, IEnumerable<GetRecurrencesResponse> recurrences)
     {
         Date = date;
         Recurrences = recurrences;
+    }
+
+    [RelayCommand]
+    public void TestMe()
+    {
+        int x = 10;
+    }
+
+    [RelayCommand]
+    public async void MarkComplete(GetRecurrencesResponse? recurrenceResponse)
+    {
+        if (recurrenceResponse is null) return;
+
+        if (recurrenceResponse.Completed.HasValue && recurrenceResponse.Completed.Value == true)
+        {
+            // save the completion
+            //var r = await _eventActionServices.SaveEventCompletionAsync(recurrenceResponse.Event.Id.Value, recurrenceResponse.OccursOn.Value);
+            _eventActionServices.SaveEventCompletionAsync(recurrenceResponse.Event.Id.Value, recurrenceResponse.OccursOn.Value);
+        }
+        else
+        {
+            // remove the completion
+            //var r = await _eventActionServices.DeleteEventCompletionAsync(recurrenceResponse.Event.Id.Value, recurrenceResponse.OccursOn.Value);
+            _eventActionServices.DeleteEventCompletionAsync(recurrenceResponse.Event.Id.Value, recurrenceResponse.OccursOn.Value);
+        }
     }
 }
