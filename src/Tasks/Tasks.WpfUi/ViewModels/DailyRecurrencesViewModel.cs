2a4436b5d45fc996f1233aaebc343369f8ecb29f
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-09-27 11:07:38 -0500

    Branch issue 140 (#142)

---
+++ src/Tasks/Tasks.WpfUi/ViewModels/DailyRecurrencesViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tasks.Domain.Views;
+
+namespace Tasks.WpfUi.ViewModels;
+
+public partial class DailyRecurrencesViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private DateTime _date = DateTime.Today;
+
+    [ObservableProperty]
+    private IEnumerable<GetRecurrencesResponse> _recurrences = new List<GetRecurrencesResponse>();
+
+    [ObservableProperty]
+    private bool _isExpanded = true;
+
+    public DailyRecurrencesViewModel(DateTime date, IEnumerable<GetRecurrencesResponse> recurrences)
+    {
+        Date = date;
+        Recurrences = recurrences;
+    }
+}
---
+++ src/Tasks/Tasks.WpfUi/Views/Controls/DailyRecurrencesControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+using Tasks.WpfUi.ViewModels;
+
+namespace Tasks.WpfUi.Views.Controls;
+
+/// <summary>
+/// Interaction logic for DailyRecurrencesControl.xaml
+/// </summary>
+public partial class DailyRecurrencesControl : UserControl
+{
+    public DailyRecurrencesViewModel ViewModel { get; set; }
+
+    public DailyRecurrencesControl(DailyRecurrencesViewModel viewModel)
+    {
+        ViewModel = viewModel;
+
+        InitializeComponent();
+    }
+}
