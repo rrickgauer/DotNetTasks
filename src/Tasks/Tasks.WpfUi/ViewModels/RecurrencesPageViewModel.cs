b6d7e3109e1d712ad36f347582f2698d73e8de6d
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-09-27 10:36:02 -0500

    split the recurrences into their appropriate days

--- src/Tasks/Tasks.WpfUi/ViewModels/RecurrencesPageViewModel.cs
+++ src/Tasks/Tasks.WpfUi/ViewModels/RecurrencesPageViewModel.cs
@@ -10,6 +10,7 @@ using Tasks.Services.Interfaces;
 using Tasks.Utilities;
 using Tasks.Validation;
 using Tasks.WpfUi.Services;
+using Tasks.WpfUi.Views.Controls;
 using Wpf.Ui.Common.Interfaces;
 
 namespace Tasks.WpfUi.ViewModels;
@@ -43,9 +44,9 @@ public partial class RecurrencesPageViewModel : ObservableObject, INavigationAwa
 
 
     [ObservableProperty]
-    private IEnumerable<GetRecurrencesResponse> _recurrences = new List<GetRecurrencesResponse>();
+    private IEnumerable<DailyRecurrencesControl> _recurrences = new List<DailyRecurrencesControl>();
 
-    partial void OnRecurrencesChanged(IEnumerable<GetRecurrencesResponse> value)
+    partial void OnRecurrencesChanged(IEnumerable<DailyRecurrencesControl> value)
     {
         IsLoading = false;
     }
@@ -65,12 +66,43 @@ public partial class RecurrencesPageViewModel : ObservableObject, INavigationAwa
     public void OnNavigatedTo() { }
     #endregion
 
+    /// <summary>
+    /// Set the weekly recurrences 
+    /// </summary>
+    /// <param name="date"></param>
+    /// <returns></returns>
     public async Task LoadRecurrences(DateTime date)
     {
-        Recurrences = await GetRecurrencesAsync(date);
+        // get all the user's recurrences for the week
+        var recurrencesList = await GetRecurrencesAsync(date);
+
+        // get the week's starting/ending date
+        ValidDateRange range = date.GetDateRangeFromWeek(DayOfWeek.Monday);
+
+        List<DailyRecurrencesControl> controls = new();
+        
+        // separate the recurrences collection into each day of the week
+        for (DateTime d = range.StartsOn; d != range.EndsOn; d = d.AddDays(1))
+        {
+            // get the recurrences for the date
+            var recurrencesToday = recurrencesList.Where(r => r.OccursOn == d).ToList();
+
+            // setup a new view model for the daily recurrences control
+            DailyRecurrencesViewModel viewModel = new(d, recurrencesToday);
+
+            // add a new control to the collection
+            controls.Add(new(viewModel));
+        }
+
+        // update the UI
+        Recurrences = controls;
     }
 
-
+    /// <summary>
+    /// Fetch the user's recurrences that are occur in the week of the given date
+    /// </summary>
+    /// <param name="date"></param>
+    /// <returns></returns>
     private async Task<IEnumerable<GetRecurrencesResponse>> GetRecurrencesAsync(DateTime date)
     {
         ValidDateRange range = date.GetDateRangeFromWeek(DayOfWeek.Monday);
