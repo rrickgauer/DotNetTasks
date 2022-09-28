1da0f9fb3aa944c9126413c948cf0f1e7cea33e4
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-09-28 12:57:20 -0500

    render the labels

--- src/Tasks/Tasks.WpfUi/ViewModels/LabelsPageViewModel.cs
+++ src/Tasks/Tasks.WpfUi/ViewModels/LabelsPageViewModel.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tasks.Domain.Models;
+using Tasks.Services.Interfaces;
+using Tasks.WpfUi.Services;
 using Wpf.Ui.Common.Interfaces;
 
 namespace Tasks.WpfUi.ViewModels;
@@ -18,12 +21,41 @@ public partial class LabelsPageViewModel : ObservableObject, INavigationAware
 
     public void OnNavigatedTo()
     {
-        //throw new NotImplementedException();
+        LoadLabelsAsync();
     }
     #endregion
 
 
+    private readonly ILabelServices _labelServices;
+    private readonly WpfApplicationServices _applicationServices;
+
+    public LabelsPageViewModel(ILabelServices labelServices, WpfApplicationServices applicationServices)
+    {
+        _labelServices = labelServices;
+        _applicationServices = applicationServices;
+    }
+
+    [ObservableProperty]
+    private bool _showProgress = true;
+
+    [ObservableProperty]
+    private bool _showLabels = false;
+
+    [ObservableProperty]
+    private List<Label> _labels = new();
 
 
+    public async void LoadLabelsAsync()
+    {
+        ShowProgress = true;
+        ShowLabels = false;
+
+        var labels = (await _labelServices.GetLabelsAsync(_applicationServices.User.Id.Value)).Data;
+        labels ??= new List<Label>();
+        Labels = labels.ToList();
+
+        ShowProgress = false;
+        ShowLabels = true;
+    }
 
 }
