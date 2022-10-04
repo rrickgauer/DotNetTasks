a8f144fd1240b99b2f7ecf148345fac90d1c5758
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-10-04 16:13:04 -0500

    add functionality to go back button

---
+++ src/Tasks/Tasks.WpfUi/Helpers/TimeSpanToDateTimeConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Tasks.WpfUi.Helpers;
+
+public class TimeSpanToDateTimeConverter : IValueConverter
+{
+    /// <summary>
+    /// Convert the timespan to a datetime
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="targetType"></param>
+    /// <param name="parameter"></param>
+    /// <param name="culture"></param>
+    /// <returns></returns>
+    public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null) return null;
+
+        var timeSpan = (TimeSpan)value;
+        
+        var result = new DateTime(timeSpan.Ticks);
+
+        return result;
+    }
+
+    /// <summary>
+    /// Convert datetime to a timespan
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="targetType"></param>
+    /// <param name="parameter"></param>
+    /// <param name="culture"></param>
+    /// <returns></returns>
+    public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is null) return null;
+
+        var datetime = (DateTime)value;
+        return datetime.TimeOfDay;
+    }
+}
--- src/Tasks/Tasks.WpfUi/ViewModels/ViewEventPageViewModel.cs
+++ src/Tasks/Tasks.WpfUi/ViewModels/ViewEventPageViewModel.cs
@@ -1,18 +1,14 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
 using Tasks.Domain.Models;
 using Tasks.WpfUi.Services;
-using Tasks.WpfUi.Views.Pages;
+using Wpf.Ui.Common.Interfaces;
 using Wpf.Ui.Controls.Interfaces;
 using Wpf.Ui.Mvvm.Contracts;
 
 namespace Tasks.WpfUi.ViewModels;
 
-public partial class ViewEventPageViewModel : ObservableObject
+public partial class ViewEventPageViewModel : ObservableObject, INavigationAware
 {
     private readonly WpfApplicationServices _applicationServices;
     private readonly INavigation _navigation = App.GetService<INavigationService>().GetNavigationControl();
@@ -22,6 +18,36 @@ public partial class ViewEventPageViewModel : ObservableObject
         _applicationServices = applicationServices;
     }
 
+    /// <summary>
+    /// The current event
+    /// </summary>
     [ObservableProperty]
     private Event? _event;
+
+
+    private INavigationItem? _previousNavigationItem = null;
+
+    [RelayCommand]
+    public async void GoBack()
+    {
+        if (_previousNavigationItem is null) return;
+
+        _navigation.Navigate(_previousNavigationItem.PageTag);
+    }
+
+    #region INavigationAware
+
+    /// <summary>
+    /// Record the page from which this page was navigated from
+    /// </summary>
+    public void OnNavigatedTo()
+    {
+        _previousNavigationItem = _navigation.Current;
+    }
+
+    public void OnNavigatedFrom()
+    {
+        //throw new System.NotImplementedException();
+    }
+    #endregion
 }
