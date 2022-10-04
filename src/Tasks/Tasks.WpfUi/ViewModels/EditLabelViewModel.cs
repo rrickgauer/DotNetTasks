e4cf97aa31a97a7af7abc66ebecffdf5a9e01710
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-10-03 20:21:55 -0500

    go back to labels page

--- src/Tasks/Tasks.WpfUi/ViewModels/EditLabelViewModel.cs
+++ src/Tasks/Tasks.WpfUi/ViewModels/EditLabelViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,10 @@ using System.Threading.Tasks;
 using Tasks.Domain.Models;
 using Tasks.Services.Interfaces;
 using Tasks.WpfUi.Services;
+using Tasks.WpfUi.Views.Pages;
 using Wpf.Ui.Common.Interfaces;
+using Wpf.Ui.Controls.Interfaces;
+using Wpf.Ui.Mvvm.Contracts;
 
 namespace Tasks.WpfUi.ViewModels;
 
@@ -27,7 +31,7 @@ public partial class EditLabelViewModel : ObservableObject, INavigationAware
 
     private readonly ILabelServices _labelServices;
     private readonly WpfApplicationServices _applicationServices;
-
+    private readonly INavigation _navigation = App.GetService<INavigationService>().GetNavigationControl();
 
     public EditLabelViewModel(ILabelServices labelServices, WpfApplicationServices applicationServices)
     {
@@ -38,5 +42,13 @@ public partial class EditLabelViewModel : ObservableObject, INavigationAware
     [ObservableProperty]
     private Label? _label;
 
+    /// <summary>
+    /// Go back to the labels page
+    /// </summary>
+    [RelayCommand]
+    public async void GoBackToLabelsPage()
+    {
+        _navigation.Navigate(typeof(LabelsPage));
+    }
 
 }
