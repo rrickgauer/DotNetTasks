eba8381bd30f388464908bf1ad5825c351006803
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-10-11 10:31:51 -0500

    bind the password text inputs

--- src/Tasks/Tasks.WpfUi/ViewModels/AccountPageViewModel.cs
+++ src/Tasks/Tasks.WpfUi/ViewModels/AccountPageViewModel.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tasks.Domain.Models;
+using Tasks.Domain.Views;
+using Tasks.Services.Interfaces;
 using Tasks.WpfUi.Services;
 using Wpf.Ui.Common.Interfaces;
 
@@ -17,19 +20,68 @@ public partial class AccountPageViewModel : ObservableObject, INavigationAware
         //throw new NotImplementedException();
     }
 
-    public void OnNavigatedTo()
+    public async void OnNavigatedTo()
     {
-        //LoadLabelsAsync();
+        IsLoading = true;
+
+        await LoadUserData();
+
+        IsLoading = false;
     }
     #endregion
 
-
     private readonly WpfApplicationServices _applicationServices;
+    private readonly IUserServices _userServices;
 
-    public AccountPageViewModel(WpfApplicationServices applicationServices)
+    private GetUserResponse _user;
+
+    public AccountPageViewModel(WpfApplicationServices applicationServices, IUserServices userServices)
     {
         _applicationServices = applicationServices;
+        _userServices = userServices;
+
+        LoadUserData();
     }
 
+
+    #region Password values
+
+    [ObservableProperty]
+    private string? _currentPassword = null;
+
+    [ObservableProperty]
+    private string? _newPassword = null;
+
+    [ObservableProperty]
+    private string? _confirmPassword = null;
+
+    #endregion
+
+    [ObservableProperty]
+    private bool _isLoading = false;
+
+
+    [ObservableProperty]
+    private bool _isReceiveDailyRemindersChecked;
+
+    partial void OnIsReceiveDailyRemindersCheckedChanged(bool value)
+    {
+        IsUpdateEmailPreferencesButtonEnabled = true;
+    }
+
+
+    [ObservableProperty]
+    private bool _isUpdateEmailPreferencesButtonEnabled = false;
+
+
+
+    public async Task LoadUserData()
+    {
+        _user = await _userServices.GetUserViewAsync(_applicationServices.User.Id.Value) ?? new();
+
+        IsReceiveDailyRemindersChecked = _user.DeliverReminders ?? false;
+
+        IsUpdateEmailPreferencesButtonEnabled = false;
+    }
 
 }
