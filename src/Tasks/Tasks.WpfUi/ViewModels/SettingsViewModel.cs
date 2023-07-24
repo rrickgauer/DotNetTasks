cdd7cdc02ae87abb3447051b7e1d36b689c52cdf
Ryan Rickgauer <rrickgauer1@gmail.com>
2023-07-24 13:59:38 -0500

    upgrade to .net 7, update nuget packages, remove deprecated packages

--- src/Tasks/Tasks.WpfUi/ViewModels/SettingsViewModel.cs
+++ src/Tasks/Tasks.WpfUi/ViewModels/SettingsViewModel.cs
@@ -1,83 +1,81 @@
-﻿using CommunityToolkit.Mvvm.Input;
-using Microsoft.Toolkit.Mvvm.ComponentModel;
-using Microsoft.Toolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Windows.Input;
 using Tasks.WpfUi.Services;
 using Wpf.Ui.Common.Interfaces;
 
-namespace Tasks.WpfUi.ViewModels
+namespace Tasks.WpfUi.ViewModels;
+
+public partial class SettingsViewModel : ObservableObject, INavigationAware
 {
-    public partial class SettingsViewModel : ObservableObject, INavigationAware
+    private bool _isInitialized = false;
+
+    [ObservableProperty]
+    private string _appVersion = string.Empty;
+
+    [ObservableProperty]
+    private Wpf.Ui.Appearance.ThemeType _currentTheme = Wpf.Ui.Appearance.ThemeType.Unknown;
+
+
+    private readonly WpfApplicationServices _applicationServices;
+
+    public SettingsViewModel(WpfApplicationServices applicationServices)
     {
-        private bool _isInitialized = false;
+        _applicationServices = applicationServices;
+    }
 
-        [ObservableProperty]
-        private string _appVersion = string.Empty;
+    public void OnNavigatedTo()
+    {
+        if (!_isInitialized)
+            InitializeViewModel();
+    }
 
-        [ObservableProperty]
-        private Wpf.Ui.Appearance.ThemeType _currentTheme = Wpf.Ui.Appearance.ThemeType.Unknown;
+    public void OnNavigatedFrom()
+    {
+    }
 
+    private void InitializeViewModel()
+    {
+        CurrentTheme = Wpf.Ui.Appearance.Theme.GetAppTheme();
+        AppVersion = $"Tasks.WpfUi - {GetAssemblyVersion()}";
 
-        private readonly WpfApplicationServices _applicationServices;
+        _isInitialized = true;
+    }
 
-        public SettingsViewModel(WpfApplicationServices applicationServices)
+    private string GetAssemblyVersion()
+    {
+        return System.Reflection.Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? string.Empty;
+    }
+
+    [RelayCommand]
+    private void OnChangeTheme(string parameter)
+    {
+        switch (parameter)
         {
-            _applicationServices = applicationServices;
-        }
-
-        public void OnNavigatedTo()
-        {
-            if (!_isInitialized)
-                InitializeViewModel();
-        }
-
-        public void OnNavigatedFrom()
-        {
-        }
-
-        private void InitializeViewModel()
-        {
-            CurrentTheme = Wpf.Ui.Appearance.Theme.GetAppTheme();
-            AppVersion = $"Tasks.WpfUi - {GetAssemblyVersion()}";
-
-            _isInitialized = true;
-        }
-
-        private string GetAssemblyVersion()
-        {
-            return System.Reflection.Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? string.Empty;
-        }
-
-        [ICommand]
-        private void OnChangeTheme(string parameter)
-        {
-            switch (parameter)
-            {
-                case "theme_light":
-                    if (CurrentTheme == Wpf.Ui.Appearance.ThemeType.Light)
-                        break;
-
-                    Wpf.Ui.Appearance.Theme.Apply(Wpf.Ui.Appearance.ThemeType.Light);
-                    CurrentTheme = Wpf.Ui.Appearance.ThemeType.Light;
-
+            case "theme_light":
+                if (CurrentTheme == Wpf.Ui.Appearance.ThemeType.Light)
                     break;
 
-                default:
-                    if (CurrentTheme == Wpf.Ui.Appearance.ThemeType.Dark)
-                        break;
+                Wpf.Ui.Appearance.Theme.Apply(Wpf.Ui.Appearance.ThemeType.Light);
+                CurrentTheme = Wpf.Ui.Appearance.ThemeType.Light;
 
-                    Wpf.Ui.Appearance.Theme.Apply(Wpf.Ui.Appearance.ThemeType.Dark);
-                    CurrentTheme = Wpf.Ui.Appearance.ThemeType.Dark;
+                break;
 
+            default:
+                if (CurrentTheme == Wpf.Ui.Appearance.ThemeType.Dark)
                     break;
-            }
-        }
 
-        [RelayCommand]
-        public void Logout()
-        {
-            _applicationServices.Logout();
+                Wpf.Ui.Appearance.Theme.Apply(Wpf.Ui.Appearance.ThemeType.Dark);
+                CurrentTheme = Wpf.Ui.Appearance.ThemeType.Dark;
+
+                break;
         }
+    }
+
+    [RelayCommand]
+    public void Logout()
+    {
+        _applicationServices.Logout();
     }
 }
