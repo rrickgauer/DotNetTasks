f5c08f747bee51a4ab4f7c24fe2bdb33cbbd4756
Ryan Rickgauer <rrickgauer1@gmail.com>
2023-07-24 19:37:13 -0500

    remove old sql ddl files

--- sql/Tables/Event_Actions.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Event_Actions (
-    event_id CHAR(36) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-    on_date DATE NOT NULL,
-    event_action_type_id SMALLINT UNSIGNED NOT NULL,
-    created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (event_id, on_date, event_action_type_id),
-    FOREIGN KEY (event_id) REFERENCES Events(id) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (event_action_type_id) REFERENCES Event_Action_Types(id) ON UPDATE CASCADE
-) ENGINE=INNODB;
--- sql/Tables/Users.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `Users` (
-    `id` CHAR(36) COLLATE utf8_unicode_ci NOT NULL,
-    `email` VARCHAR(100) COLLATE utf8_unicode_ci NOT NULL,
-    `password` VARCHAR(250) COLLATE utf8_unicode_ci NOT NULL,
-    `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `deliver_reminders` BOOLEAN NOT NULL DEFAULT FALSE PRIMARY KEY (`id`),
-    UNIQUE KEY `id` (`id`),
-    UNIQUE KEY `email` (`email`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci;
