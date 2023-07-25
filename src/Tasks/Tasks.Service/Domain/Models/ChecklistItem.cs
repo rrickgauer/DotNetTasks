a4160e4f45ce054204a3355e3030eaf6e00ebf6e
Ryan Rickgauer <rrickgauer1@gmail.com>
2023-07-25 18:21:53 -0500

    Add checklist domain models (#201)

---
+++ src/Tasks/Tasks.Service/Domain/Models/ChecklistItem.cs
@@ -0,0 +1,37 @@
+﻿using Tasks.Service.CustomAttributes;
+
+namespace Tasks.Service.Domain.Models;
+
+public class ChecklistItem
+{
+    [SqlColumn("id")]
+    public Guid? Id { get; set; }
+
+    [SqlColumn("checklist_id")]
+    public Guid? ListId { get; set; }
+
+    [SqlColumn("content")]
+    public string? Content { get; set; }
+
+    [SqlColumn("created_on")]
+    public DateTime CreatedOn { get; set; } = DateTime.Now;
+
+    [SqlColumn("completed_on")]
+    public DateTime? CompletedOn { get; set; } = null;
+
+    [SqlColumn("position")]
+    public uint Position { get; set; } = 0;
+
+
+
+    /// <summary>
+    /// Flag to check if item is complete.
+    /// If CompletedOn has a value (is not null), then the item is complete.
+    /// Otherwise, it is incomplete.
+    /// </summary>
+    public bool IsComplete
+    {
+        get => CompletedOn != null;
+        set => CompletedOn = value ? DateTime.Now : null;
+    }
+}
