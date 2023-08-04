9be0d9ca0bd290686d9d79cf8efbf7e6c272b8c2
Ryan Rickgauer <rrickgauer1@gmail.com>
2023-08-03 21:16:17 -0500

    display link in alert to view the checklist

--- src/Gui/tasks/static/js/custom/pages/checklist-settings/page-controller.js
+++ src/Gui/tasks/static/js/custom/pages/checklist-settings/page-controller.js
@@ -1,5 +1,6 @@
 import { AlertPageTopBase, AlertPageTopDanger, AlertPageTopSuccess } from "../../components/page-alerts/alert-page-top";
 import { ChecklistSettingsChecklistClonedEvent, ChecklistSettingsChecklistDeletedEvent, ChecklistSettingsGeneralFormSubmittedEvent } from "../../domain/events/events";
+import { ChecklistModel } from "../../domain/models/checklist";
 import { UrlMethods } from "../../helpers/url-methods";
 import { CloneChecklistController } from "./clone-checklist-controller";
 import { DeleteChecklistController } from "./delete-checklist-controller";
@@ -40,28 +41,43 @@ export class ChecklistSettingsPageController
 
         ChecklistSettingsChecklistClonedEvent.addListener((e) => 
         {
-            const successAlert = new AlertPageTopSuccess("Checklist was successfully cloned");
-            successAlert.show();
+            this.#handleSuccessfulChecklistClone(e.data);
         });
         
     }
 
     #onChecklistSettingsGeneralFormSubmittedEvent = (successful) =>
     {
-
         /** @type {AlertPageTopBase} */
-        let customAlert = null;
+        let customAlert = new AlertPageTopSuccess('Changes saved successfully');
 
-        if (successful)
-        {
-            customAlert = new AlertPageTopSuccess('Changes saved successfully');
-        }
-        else
+        if (!successful)
         {
             customAlert = new AlertPageTopDanger('There was an error saving the checklist. Please try again');
         }
 
         customAlert.show();
+    }
+
+
+    /**
+     * Handle a successful clone
+     * @param {ChecklistModel} newChecklist updated checklist
+     */
+    #handleSuccessfulChecklistClone = (newChecklist) =>
+    {
+        // build the url for the checklist
+        const parms = new URLSearchParams();
+        parms.set('openChecklists', newChecklist.id);
+
+        const url = `/checklists?${parms.toString()}`;
+        
+        // wrap the link in an anchor tag to display in the alert
+        const alertText = `Checklist was successfully cloned. <a href="${url}" class="alert-link">Click here to view it.</a>`;
+
+        // show the alert with the link in it
+        const successAlert = new AlertPageTopSuccess(alertText);
+        successAlert.show();
     }
 
 
