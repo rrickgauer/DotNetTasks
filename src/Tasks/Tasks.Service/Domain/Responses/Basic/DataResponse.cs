d33f549085597ba1e4d50d2d32d7bc46eaf9aa33
Ryan Rickgauer <rrickgauer1@gmail.com>
2023-08-12 20:35:28 -0500

    make new response classes

--- src/Tasks/Tasks.Service/Domain/Responses/Basic/DataResponse.cs
+++ src/Tasks/Tasks.Service/Domain/Responses/Basic/DataResponse.cs
@@ -1,10 +1,14 @@
 ﻿namespace Tasks.Service.Domain.Responses.Basic;
 
-public abstract class DataResponse<T> : IDataResponse<T>
-{
-    public bool Successful { get; set; }
-    public Exception? Exception { get; set; }
-    public string? Message { get; set; }
-    public abstract T? Data { get; set; }
+public class DataResponse<T> : BaseResponse
+{ 
+    public T? Data { get; set; }
+
+    public DataResponse() { }
+
+    public DataResponse(T? data)
+    {
+        Data = data;
+    }
 }
 
