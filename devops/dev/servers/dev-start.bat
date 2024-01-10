ed71ce7c01a4f1a7a860c69160b03110bfeacc03
Ryan Rickgauer <rrickgauer1@gmail.com>
2024-01-10 14:34:53 -0600

    Update dev-start.bat

--- devops/dev/servers/dev-start.bat
+++ devops/dev/servers/dev-start.bat
@@ -3,4 +3,6 @@
 :: --------------------------------------------
 
 cd C:\xampp\htdocs\files\DotNetTasks\devops\dev\servers
-wt -M start-server-api.bat; start-server-gui.bat ; start-css-compiler.bat ; start-js.bat
+
+
+wt -M "C:\xampp\htdocs\files\DotNetTasks\devops\dev\servers\start-server-api.bat"; "C:\xampp\htdocs\files\DotNetTasks\devops\dev\servers\start-server-gui.bat" ; "C:\xampp\htdocs\files\DotNetTasks\devops\dev\servers\start-css-compiler.bat" ; "C:\xampp\htdocs\files\DotNetTasks\devops\dev\servers\start-js.bat"
